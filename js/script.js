8d8e51f1069072af1abfceb3cf6a5f5f9a51f55d
Mariusz Kalinowski <Mariusz.Kalinowski95@gmail.com>
2023-06-18 18:28:26 +0200

    remove task button

--- js/script.js
+++ js/script.js
@@ -4,52 +4,68 @@
 
     const tasks = [];
 
-    const render = () => {
-        let htmlString = "";
-
-        for (const task of tasks) {
-            htmlString +=`
-            <li>
-             ${task.content}
-            </li>
-            `;
-        }
-
-        document.querySelector(".js-tasks").innerHTML = htmlString;
-    };
-
     const addNewTask = (newTask) => {
         tasks.push({
             content: newTask,
         });
 
         render();
-    }
+    };
+
+    const removeTask = (index) => {
+        tasks.splice(index, 1);    
+        render();
+    };
+
+
+    const render = () => {
+        let htmlString = "";
+
+        for (const task of tasks) {
+            htmlString += `
+            <li ${task.done ? "class=\"form__task--done\"" : ""}>
+            ${task.content}
+            <button class="form__removeButton js-remove">Usuń zadanie</button>
+            </li>
+        `;
+        }
+
+        document.querySelector(".js-tasks").innerHTML = htmlString;
+
+        const removeButtons = document.querySelectorAll(".js-remove");
+
+        removeButtons.forEach((removeButton, index) => {
+            removeButton.addEventListener("click", () => {
+              removeTask(index);
+            });
+        });
+    };
 
     const onFormSumbit = (event) => {
         event.preventDefault();
 
         const newTask = document.querySelector(".js-newTask").value.trim();
-        
+
         if (newTask === "") {
-          return;
+            return;
         }
 
         addNewTask(newTask);
     };
 
     const init = () => {
-            render();
+        render();
 
-            const form = document.querySelector(".js-form");
+        const form = document.querySelector(".js-form");
 
     }
 
-    
+
     init();
 
     const form = document.querySelector(".js-form");
 
     form.addEventListener("submit", onFormSumbit);
-    };
+};
+
 
